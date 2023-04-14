1f2d0ff515cce8c9d3a66c4639377d61383f0647
Francis Mensah <105157814+otcherefrancis@users.noreply.github.com>
2023-04-14 07:24:32 +0000

    Add files via upload

---
+++ I.C.T QUIZ/js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The model which has a major disadvantage in terms of the coding phase of a software life cycle model is",
+    answer: "4GT Model",
+    options: [
+      "Rad Model",
+      "Spiral Model",
+      "4GT Model",
+      "Waterfall Model"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following is not a software process quality?",
+    answer: "Portability",
+    options: [
+      "Visibility",
+      "Timeliness",
+      "Productivity",
+      "Portability"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following are CASE tools?",
+    answer: "All of the mentioned",
+    options: [
+      "Central Repository",
+      " Integrated Case Tools",
+      "Upper Case Tools",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who proposed the spiral model?",
+    answer: "Barry Boehm",
+    options: [
+      "Barry Boehm",
+      "Pressman",
+      "Royce",
+      "IBM"
+    ]
+  },
+    {
+    numb: 5,
+    question: " What does SDLC stands for?",
+    answer: " Software Development Life Cycle",
+    options: [
+      " Software Development Life Cycle",
+      "System Design Life Cycle",
+      "Software Design Life Cycle",
+      "System Development Life cycle"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: " Attributes of good software is?",
+    answer: "Maintainability & functionality",
+    options: [
+      "Development",
+      "Maintainability & functionality",
+      "Functionality",
+      "Maintainability"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "is defined as the process of generating analysis and designing documents?",
+    answer: "Reverse engineering",
+    options: [
+      "Reverse engineering",
+      "Re-engineering",
+      "Software re-engineering",
+      "Science and engineering"
+    ]
+  },
+  {
+    numb: 9,
+    question: "CASE stands for?",
+    answer: "Computer-Aided Software Engineering",
+    options: [
+      "Cost Access System Engineering",
+      "Cost Aided System Experiments",
+      "Control Aided Science and Engineering",
+      "Computer-Aided Software Engineering"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Define Agile scrum methodology?",
+    answer: "project management that emphasizes incremental progress",
+    options: [
+      "project management that emphasizes incremental progress",
+      "project management that emphasizes decremental progress",
+      "project management that emphasizes neutral progress",
+      "project management that emphasizes no progress"
+    ]
+  }
+];
